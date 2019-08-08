12a9d008690e5e882be1e2f812444e0b7dbac43e
Maellin <53312116+Maellin@users.noreply.github.com>
2019-08-08 09:44:56 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,11 +3,11 @@
 if (isset($_POST['envoyer-mail']))
 {
 	mail ("mozzi.ornella@gmail.com" , "Contact de la part de ".$_POST['nom'] , $_POST['message']."\r"."De la part de ".$_POST['mail']);
-	redirect('index.php?mail-sent=true');
+	header('Location: index.php?mail-sent=true');
 }
 
 else
 {
-	redirect('index.php?mail-failed=true');
+	header('Location: index.php?mail-failed=true');
 }
 ?>
