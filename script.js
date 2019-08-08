e7146bd18357ed2dbaae3d11c75bde05be59aada
Maellin <53312116+Maellin@users.noreply.github.com>
2019-08-08 09:24:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-center",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
+
 var show = function(url) {
 	var backdrop = document.getElementById('backdrop');
 	backdrop.style.display = 'flex';
