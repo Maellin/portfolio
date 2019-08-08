1f2a7f449b69cc93b1fa19eb5ee81c78f26d55d5
Maellin <53312116+Maellin@users.noreply.github.com>
2019-08-08 09:32:22 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,13 @@
 <?php
 
-    include 'index.php';
-
-
 if (isset($_POST['envoyer-mail']))
 {
 	mail ("mozzi.ornella@gmail.com" , "Contact de la part de ".$_POST['nom'] , $_POST['message']."\r"."De la part de ".$_POST['mail']);
+	redirect('index.php?mail-sent=true');
 }
 
 else
 {
-	echo('Vous n\'avez pas envoyé le mail');
+	redirect('index.php?mail-failed=true');
 }
 ?>
