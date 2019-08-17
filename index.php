4053b52339e82f2e3763dc3519c71f36b0a606f0
Ornella <mozzi.ornella@gmail.com>
2019-08-17 19:36:40 +0200

    responsive 2.0

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 			include'nav_bar.php';
 		?>
 
-		<div class="container-page">
+		<div class="container-page d-flex">
+			<figure class="picture d-flex a-center">
+				<img src="./img/ornella-mozzi.png" alt="Une photo de moi, souriant"/>
+			</figure>
+			
 			<div class="container-intro d-flex j-center">
 				<p class="bold title--1">Hello !</p>
 				<p class="content">Je suis Ornella, une <strong class="bold">web designer</strong> basée à Lille, en France. Ici, vous pourrez <a href="works.php"> découvrir mes créations</a> et <a href="contact.php">me contacter</a> si vous avez la moindre question. <br/>
-				Bonne visite !</p>
-
-				<figure class="picture">
-					<img src="./img/ornella-mozzi.png" alt="Une photo de moi, souriant"/>
-				</figure>	
+				Bonne visite !</p>	
 				
 				<div class="button-container">
 					<a class="button button-link bold" href="works.php">Découvrir mes travaux</a>
